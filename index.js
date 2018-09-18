48ce8b13565993807ab64cd5f0b1af4b8796a49e
billy <billy@arkhamgc.com>
2018-09-18 01:24:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // Add your functions and code here
+
+ var kittens =["Milo","Otis","Garfield"];
+
+function destructivelyAppendKitten(name){
+    kittens.push("Ralph");
+      return kittens;
+}
+
+var kittens =["Milo","Otis","Garfield"];
+
+function destructivelyPrependKitten(name){
+    kittens.unshift("Bob");
+      return kittens;
+}
+
+var kittens =["Milo","Otis","Garfield"];
+
+function destructivelyRemoveLastKitten(name){
+    kittens.pop();
+      return kittens;
+}
+
+var kittens =["Milo","Otis","Garfield"];
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift();
+  return kittens;
+  
+}
+
+var kittens =["Milo","Otis","Garfield"];
+var nKitten =["Broom"];
+function appendKitten(array1,array2){
+   var nKit = (kittens.concat(nKitten));
+   return nKit;
+ } 
+  
+var kittens =["Milo","Otis","Garfield"];
+var nKitten =["Broom"];
+function prependKitten(name){
+   var nKit = (nKitten.concat(Kittens));
+   return nKit;
+ } 
+  
+
